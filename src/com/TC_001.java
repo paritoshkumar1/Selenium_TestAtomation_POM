05bbe86209ec48c0ef63d542baa817f07a8af6fb
Paritosh Kumar <paritosh.kumar307@gmail.com>
2018-04-09 20:31:08 +0530

    Initial commit - Selenium_TestAtomation_POM

---
+++ src/com/TC_001.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	@Test
+	
+	public void m1(){
+		
+   ChromeDriver driver=new ChromeDriver();
+   driver.get("https://www.google.co.in");
+   
+   
+   
+   
+	}
+
+}
---
+++ src/com/TC_002.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_002 {
+	
+	@Test
+	
+	public void m2(){
+		
+   ChromeDriver driver=new ChromeDriver();
+   driver.get("https://www.google.co.in");
+ 
+		
+   
+	}
+
+}
